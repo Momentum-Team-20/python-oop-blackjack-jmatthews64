e28c3eed68c230b285b58e7089ead19441cb33b3
jmatthews64 <jon.matthews64@gmail.com>
2023-11-07 10:10:31 -0500

    Updating code to reflect what was typed in class as of 10am Tue

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 # Write your blackjack game here.
 # C - card
 # R - value, suit, rank, know where it is in deck
@@ -37,11 +39,25 @@ class Deck:
                 self.cards.append(card)
         print(f'There are {len(self.cards)} in this deck.')
 
+    def shuffle(self):
+        random.shuffle(self.cards)
+
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
+
+    def __str__(self):
+        return self.name
+
+    def look_at_hand(self):
+        '''Shows cards in hand to player'''
+        pass
+
+    def calculate_hand_value(self):
+        '''return the total value of the hand'''
+        pass
 
 
 class Dealer(Player):
@@ -53,15 +69,30 @@ class Dealer(Player):
 class Game:
     def __init__(self):
         self.deck = Deck()
+        self.deck.shuffle()
         self.player = Player(name=input("What is your name? "))
         self.dealer = Dealer()
+        self.deal_cards()
 
 # ace_of_spades = Card(suit='♠️', rank='A', value=(1, 11))
 # four_of_clubs = Card(suit='♣️', rank=4, value=4)
+    def deal_cards(self):
+        '''Deal 2 cards each to the player and the dealer '''
+        while len(self.player.hand) < 2 and len(self.dealer.hand) < 2:
+            card = self.deck.cards.pop()
+            self.player.hand.append(card)
+            card = self.deck.cards.pop()
+            self.dealer.hand.append(card)
+        print(f'{self.player} has {self.player.hand} \n'
+              f'and {self.dealer} has {self.dealer.hand}')
 
+    def hit(self, player):
+        '''Deal one card to the selected player'''
+        pass
 
 # print(ace_of_spades)
 # print(four_of_clubs)
 
+
 new_game = Game()
-breakpoint()
+# TODO Add values of each hand so player and dealer can decide to hit or stay
